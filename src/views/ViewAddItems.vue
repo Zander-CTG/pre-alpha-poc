d235a7bb06dd638290eef42593dd38fa2b4739ac
michael-255 <kodiakchaos@gmail.com>
2025-04-29 09:33:31 -0400

    test call of edge function

--- src/views/ViewAddItems.vue
+++ src/views/ViewAddItems.vue
@@ -1,12 +1,52 @@
 <script setup lang="ts">
 import PageHeading from '@/components/PageHeading.vue'
+import useLogger from '@/composables/useLogger'
 import { appTitle } from '@/shared/constants'
 import { uploadIcon } from '@/shared/icons'
+import { useBackendStore } from '@/stores/backend'
 import { useMeta } from 'quasar'
 
 useMeta({ title: `${appTitle} | Add Items` })
+
+const { log } = useLogger()
+const backendStore = useBackendStore()
+// const settingsStore = useSettingsStore()
+
+async function onHello() {
+  try {
+    const {
+      data: { session },
+      error: sessionError,
+    } = await backendStore.supabase?.auth.getSession()
+
+    if (sessionError || !session) {
+      log.error('No user session found', sessionError)
+      return
+    }
+
+    const { data, error } = await backendStore.supabase?.functions.invoke('hello-world', {
+      headers: {
+        Authorization: `Bearer ${session.access_token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    log.info('Edge function called', { data, error })
+  } catch (error) {
+    log.error('Error in onHello:', error as Error)
+  }
+}
 </script>
 
 <template>
   <PageHeading :icon="uploadIcon" title="Add Items" />
+
+  <q-list padding>
+    <q-item>
+      <q-item-section top>
+        <q-item-label>Edge Functions</q-item-label>
+        <q-btn label="Hello" color="primary" @click="onHello()" />
+      </q-item-section>
+    </q-item>
+  </q-list>
 </template>
